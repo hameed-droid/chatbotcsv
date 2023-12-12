b572973120d690bf1dc693992f590365efd08c66
hameed-droid <hameed.mitwa1@gmail.com>
2023-12-12 13:05:19 +0500

    first commit

---
+++ src/Home.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+
+#Config
+st.set_page_config(layout="wide", page_icon="🤖", page_title="Titan ")
+
+
+#Title
+st.markdown(
+    """
+    <h2 style='text-align: center;'>Titan, your assistant 🤖</h1>
+    """,
+    unsafe_allow_html=True,)
+
+st.markdown("---")
+
+
+#Description
+st.markdown(
+    """ 
+    <h5 style='text-align:center;'>I'm Titan, an intelligent chatbot . I use large language models to provide
+    context-sensitive interactions. My goal is to help you better understand your data.
+    I support PDF, TXT, CSV</h5>
+    """,
+    unsafe_allow_html=True)
+st.markdown("---")
+
+
+
+
+
